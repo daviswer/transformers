29677991983878d71d73215dfe308bfa2ad08906
Davis Wertheimer <dww78@cornell.edu>
2025-02-03 11:34:02 -0500

    Force override rope params for prior ckps

--- src/transformers/models/bamba/modeling_bamba.py
+++ src/transformers/models/bamba/modeling_bamba.py
@@ -125,6 +125,9 @@ class HybridMambaAttentionDynamicCache(modeling_jamba.HybridMambaAttentionDynami
 class BambaRotaryEmbedding(nn.Module):
     def __init__(self, config: BambaConfig, device=None):
         super().__init__()
+        # Force override prior rope/length params
+        config.rope_scaling = {"rope_type":"abf"}
+        config.max_position_embeddings = 4096
         # BC: "rope_type" was originally "type"
         if hasattr(config, "rope_scaling") and config.rope_scaling is not None:
             self.rope_type = config.rope_scaling.get("rope_type", config.rope_scaling.get("type"))
