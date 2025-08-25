7bc60f34472520d83c0ac7ac28ebbcdcc28e1769
Davis Wertheimer <dww78@cornell.edu>
2025-08-25 18:38:03 -0400

    Add upi dynamic scaling

--- src/transformers/models/bamba/modeling_bamba.py
+++ src/transformers/models/bamba/modeling_bamba.py
@@ -550,13 +550,23 @@ class BambaMixer(nn.Module):
 
             # 3. SSM transformation
             A = -torch.exp(self.A_log.float())  # (nheads,)
-            A = A[:, None, ...][:, :, None].expand(-1, self.head_dim, self.ssm_state_size).to(dtype=torch.float32)
-            dt = dt[:, :, None].expand(-1, -1, self.head_dim)
-            dt_bias = self.dt_bias[:, None, ...].expand(-1, self.head_dim)
+            A = A[:, None, ...][:, :, None].expand(-1, self.head_dim, self.ssm_state_size).to(dtype=torch.float32)  # h d 128
+            dt = dt[:, :, None].expand(-1, -1, self.head_dim)  # b h d
+            dt_bias = self.dt_bias[:, None, ...].expand(-1, self.head_dim)  # h d
             D = self.D[:, None, ...].expand(-1, self.head_dim)
             B = B.view(batch_size, self.n_groups, B.shape[1] // self.n_groups)
             C = C.view(batch_size, self.n_groups, C.shape[1] // self.n_groups)
-            hidden_states_reshaped = hidden_states.view(batch_size, self.num_heads, self.head_dim)
+            hidden_states_reshaped = hidden_states.view(batch_size, self.num_heads, self.head_dim)  # b h d
+
+            # TODO
+            scalefactor = 8
+            dt = F.softplus(dt + dt_bias.to(dtype=dt.dtype))  # b h d
+            forget = dt.mul(A[:,:,0]).float().exp()  # b h d
+            # x target: (forget**(1/scale)-1)/(forget-1)*scale
+            xfactor = scalefactor * (1-forget.pow(1/scalefactor)) / (1-forget).add(1e-6)
+            dt = dt / scalefactor
+            hidden_states_reshaped = hidden_states_reshaped * xfactor.to(dtype=hidden_states_reshaped.dtype)
+            
             hidden_states = selective_state_update(
                 cache_params.ssm_states[self.layer_idx],
                 hidden_states_reshaped,
@@ -566,8 +576,8 @@ class BambaMixer(nn.Module):
                 C,
                 D,
                 z=None,
-                dt_bias=dt_bias,
-                dt_softplus=True,
+                dt_bias=None,
+                dt_softplus=False,
             )
             hidden_states = hidden_states.view(batch_size, self.num_heads * self.head_dim)
             hidden_states = self.norm(hidden_states, gate)
@@ -580,7 +590,7 @@ class BambaMixer(nn.Module):
             dt_limit_kwargs = {} if self.time_step_limit == (0.0, float("inf")) else {"dt_limit": self.time_step_limit}
 
             # 2-4. Fused kernel for conv1d, SSM, and the final projection
-            if self.training and cache_params is None:
+            if False:  # self.training and cache_params is None:
                 out = mamba_split_conv1d_scan_combined(
                     projected_states,
                     self.conv1d.weight.squeeze(1),
@@ -640,9 +650,18 @@ class BambaMixer(nn.Module):
                     dim=-1,
                 )
 
+                # TODO
+                scalefactor = 8
+                dt = F.softplus(dt + self.dt_bias.to(dtype=dt.dtype))  # b l h
+                forget = dt.mul(A).float().exp()
+                # x target: (forget**(1/scale)-1)/(forget-1)*scale
+                xfactor = scalefactor * (1-forget.pow(1/scalefactor)) / (1-forget).add(1e-6)
+                dt = dt/scalefactor
+                hidden_states = hidden_states.view(batch_size, seq_len, -1, self.head_dim) * xfactor.to(dtype=hidden_states.dtype).unsqueeze(-1)
+
                 # 3. SSM transformation
                 scan_output, ssm_state = mamba_chunk_scan_combined(
-                    hidden_states.view(batch_size, seq_len, -1, self.head_dim),
+                    hidden_states,
                     dt,
                     A,
                     B.view(batch_size, seq_len, self.n_groups, -1),
